4efb7f8fb2363a1c681bcfbd3cc9057b8fa5db0c
Andrii Lysenko <124673484+Andriy-Lysenko@users.noreply.github.com>
2025-03-27 11:41:46 +0200

    change logo source

--- app.py
+++ app.py
@@ -541,7 +541,7 @@ st.markdown(
     """
     <div class="title-container">
         <h1 style="color: white; margin: 0;">
-            <img src="https://raw.githubusercontent.com/Andriy-Lysenko/Prompt_Gen/refs/heads/main/Logo_white.png?token=GHSAT0AAAAAADAFMWJDSRA4ECXKHOUFRO5QZ7D6QOQ" width="60" height="60">
+            <img src="https://res.cloudinary.com/dts5q0ryk/image/upload/v1742992900/Logo_white_a3f4t1.png" width="60" height="60">
             Nfinite Prompt Generator
         </h1>
     </div>
